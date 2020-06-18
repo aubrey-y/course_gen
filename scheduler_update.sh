46dbcd9263d0bf1dcd79aca4ee2b44cef85689b0
aubrey-y <aubreym.yan@gmail.com>
2020-06-18 16:43:14 -0400

    data integrity fixes

--- scheduler_update.sh
+++ scheduler_update.sh
@@ -1,1 +1,1 @@
-gcloud scheduler jobs update pubsub update_classes --schedule "0 23 * * *" --topic send_refresh --message-body "test"
+gcloud scheduler jobs update pubsub update_classes --schedule "0 4 * * *" --topic send_refresh --message-body "test"
